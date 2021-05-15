77823e37c7cff7b0922b2ce09ada5a59d0be9ccc
andervyd <andervyd@gmail.com>
2021-05-16 01:52:54 +0300

    add homework lesson 1

---
+++ src/main/java/by/andervyd/acquaintance/homework/PrintQuatrain.java
@@ -0,0 +1,13 @@
+package by.andervyd.acquaintance.homework;
+
+public class PrintQuatrain {
+    public static void main(String[] args) {
+
+        System.out.println("                   РУБАИ                \n" +
+                           "Много лет размышлял я над жизнью земной.\n" +
+                           "Непонятного нет для меня под луной.\n" +
+                           "Мне известно, что мне ничего не известно! —\n" +
+                           "Вот последняя правда, открытая мной.\n" +
+                           "                                О.Хайям");
+    }
+}
